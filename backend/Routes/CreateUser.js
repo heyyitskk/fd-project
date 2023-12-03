3b414cd20c0790241c702c6db98d8e3b4f86ab31
Kunal S K <kunalkittur23@gmail.com>
2023-12-03 19:31:10 +0530

    link login and signup

--- backend/Routes/CreateUser.js
+++ backend/Routes/CreateUser.js
@@ -28,4 +28,31 @@ router.post("/createuser", [
         }
     })
 
+router.post("/loginuser", [
+    body('email').isEmail(),
+    body('password', 'Incorrect Password').isLength({ min: 5 })],
+    async (req, res) => {
+        let email = req.body.email;
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        try {
+            let userData = await User.findOne({ email });
+            if (!userData) {
+                return res.status(400).json({ errors: "Try logging with correct credentials" })
+            }
+
+            if (req.body.password !== userData.password) {
+                return res.status(400).json({ errors: "Try logging with correct credentials" })
+
+            }
+            return res.json({ success: true })
+
+        }
+        catch (error) {
+            console.log(error)
+            res.json({ success: false });
+        }
+    })
 module.exports = router; 
--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 5000
 const mongoDB = require("./db")
+const cors = require("cors")
+app.use(cors())
 mongoDB();
 app.get('/', (req, res) => {
     res.send('Hello World!!!')
