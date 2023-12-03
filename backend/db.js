0d7d109f6585c9d661cf5c0f43d655d37bdd5b9b
Kunal S K <kunalkittur23@gmail.com>
2023-12-03 17:58:19 +0530

    password stuff

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,17 @@
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb+srv://gofood:gofood@cluster0.mlxw4s3.mongodb.net/?retryWrites=true&w=majority'
-const mongoDB = () => {
-    mongoose.connect(mongoURI).then(() => {
-        console.log("Connected to mongo");
-    }).catch((e) => {
-        console.log("ERROR")
-    })
+const mongoURI = 'mongodb+srv://gofood:gofood@cluster0.mlxw4s3.mongodb.net/gofood?retryWrites=true&w=majority'
+const mongoDB = async () => {
+    try {
+        await mongoose.connect(mongoURI);
+        console.log("connected")
+        const fetched_data = await mongoose.connection.db.collection("food_items");
+        const data = await fetched_data.find({}).toArray();
+        // console.log(data)
+    }
+    catch (err) {
+        console.log(err)
+    }
+
 }
 
 module.exports = mongoDB;
