ce8126be6ae2f994e81607c2cc964461184906e5
Kunal S K <kunalkittur23@gmail.com>
2023-12-05 22:33:42 +0530

    cart functionality 2

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useDispatchCart, useCart } from './ContextReducer';
 export default function Card(props) {
-
+    let dispatch = useDispatchCart();
+    let data = useCart();
+    const priceRef = useRef();
     let options = props.options;
     let priceOptions = Object.keys(options);
-    const handleAddToCart = () => {
-
+    const [qty, setQty] = useState(1)
+    const [size, setSize] = useState("")
+    const handleAddToCart = async () => {
+        await dispatch({ type: "ADD", id: props.foodItem._id, name: props.foodItem.name, price: finalPrice, qty: qty, size: size })
+        console.log(data)
     }
+    let finalPrice = qty * parseInt(options[size]);
+    useEffect(() => {
+        setSize(priceRef.current.value)
+    }, [])
     return (
         <div><div>
             <div className="card" style={{ width: "18rem", maxHeight: "360px" }}>
@@ -21,13 +30,13 @@ export default function Card(props) {
                                 );
                             })}
                         </select>
-                        <select className="m-2 h-100 bg-success rounded">
+                        <select className="m-2 h-100 bg-success rounded" ref={priceRef} onChange={(e) => setSize(e.target.value)}>
                             {priceOptions.map((data) => {
                                 return <option key={data} value={data}>{data}</option>
                             })}
 
                         </select>
-                        <div className="d-inline h-100 fs-5">Total Price</div></div>
+                        <div className="d-inline h-100 fs-5">₹{finalPrice}/-</div></div>
                     <hr></hr>
                     <button className={`btn btn-success justify-center ms-2`} onClick={handleAddToCart}>Add to Cart</button>
                 </div>
