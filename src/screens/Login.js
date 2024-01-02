36e3161ca044322c7aa5421f79f52f67ebd94428
Kunal S K <kunalkittur23@gmail.com>
2024-01-02 20:36:54 +0530

    Sign up done

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -30,7 +30,7 @@ export default function Login() {
     setCredentials({ ...credentials, [event.target.name]: event.target.value })
   }
   return (
-    <div style={{ backgroundImage: 'url("https://images.pexels.com/photos/326278/pexels-photo-326278.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")', height: '100vh', backgroundSize: 'cover' }}>
+    <div style={{ backgroundImage: 'url("https://images.pexels.com/photos/326278/pexels-photo-326278.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")', height: '100vh', backgroundSize: 'cover', color: 'white' }}>
       <div>
         <Navbar />
       </div>
