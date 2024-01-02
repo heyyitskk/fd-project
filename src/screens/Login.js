fbf7263737854dd2116184c9649456f8eeffcef7
Kunal S K <kunalkittur23@gmail.com>
2024-01-02 20:11:53 +0530

    login page done

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import Navbar from '../components/Navbar';
 export default function Login() {
   const [credentials, setCredentials] = useState({ email: "", password: "" })
   let navigate = useNavigate();
@@ -28,23 +29,27 @@ export default function Login() {
   const onChange = (event) => {
     setCredentials({ ...credentials, [event.target.name]: event.target.value })
   }
-  return <>
-    <div className='container'>
-      <form onSubmit={handleSubmit}>
+  return (
+    <div style={{ backgroundImage: 'url("https://images.pexels.com/photos/326278/pexels-photo-326278.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")', height: '100vh', backgroundSize: 'cover' }}>
+      <div>
+        <Navbar />
+      </div>
+      <div className='container'>
+        <form className='w-50 m-auto mt-5 border bg-dark border-success rounded' onSubmit={handleSubmit}>
+          <div className="m-3">
+            <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+            <input type="email" className="form-control" name='email' value={credentials.email} onChange={onChange} aria-describedby="emailHelp" />
+            <div id="emailHelp" className="form-text">We'll never share your email with anyone.</div>
+          </div>
+          <div className="m-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+            <input type="password" className="form-control" value={credentials.password} onChange={onChange} name='password' />
+          </div>
+          <button type="submit" className="m-3 btn btn-success">Submit</button>
+          <Link to="/signup" className="m-3 mx-1 btn btn-danger">New User</Link>
+        </form>
 
-        <div className="mb-3">
-          <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name='email' value={credentials.email} onChange={onChange} />
-          <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-        </div>
-        <div className="mb-3">
-          <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-          <input type="password" className="form-control" id="exampleInputPassword1" name='password' value={credentials.password} onChange={onChange} />
-        </div>
-
-        <button type="submit" className="m-3 btn btn-success">Submit</button>
-        <Link to="/createuser" className='m-3 btn btn-danger'>I'm a new user</Link>
-      </form >
+      </div>
     </div>
-  </>;
+  )
 }
