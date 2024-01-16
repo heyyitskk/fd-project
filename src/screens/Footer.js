791c6f691d49d6a6793df2f07f1fc4dbc4bde603
Anishabhandary <anishbhandari2203@gmail.com>
2024-01-16 22:14:41 +0530

    Footer fix

--- src/screens/Footer.js
+++ src/screens/Footer.js
@@ -1,15 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 export default function Footer() {
-  return <div><footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-    <div className="col-md-4 d-flex align-items-center" style={{ margin: "0 47%" }}>
-      <Link to="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
-        <div className="mb-3 mb-md-0 text-muted">© 2023 GoFood, Inc</div>
+  return <div><footer className="py-3 border-top">
+    <div className="col-md-4" style={{ margin: "0 47%" }}>
+      <Link to="/" className="text-decoration-none lh-1">
+        <div className="text-muted">© 2023 GoFood, Inc</div>
       </Link>
     </div>
-
-    <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
-
-    </ul>
   </footer></div>;
 }
